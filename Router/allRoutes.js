921a3b646e30d99c33be947b8285b1e4ad9d4db1
Ladhari Sonia <soniamessabis@hotmail.fr>
2024-02-16 23:37:31 +0100

    Création pages users et update

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,15 +7,12 @@ export const allRoutes = [
     new Route("/connexion","Connexion","/pages/connexion.html"),
     new Route("/forgotPassword","Mot de passe oublié", "/pages/forgotPassword.html"),
     new Route("/contact","Contact","/pages/contact.html"),
+    new Route("/employeeCount", "Espace employés", "/pages/employeeCount.html"),
+    new Route("/adminCount", "Espace Administrateur", "/pages/AdminCount.html"),
+    new Route("/updateService", "Modifier un service", "/pages/updateService.html"),
+    new Route("/updateEmployee", "Modifier un employé", "/pages/updateEmployee.html"),
+    new Route("/updateVehicle", "Modifier un véhicule", "/pages/updateVehicle.html")
 ];
-
- //  new Route('/Espace employé', 'Espace employé', '/pages/espace employé.html');
- //  new Route('/Espace administrateur', 'Espace administrateur', '/pages/espace administrateur.html');
- //  new Route('/Modifier un véhicule', 'Modifier un véhicule', '/pages/modifier un véhicule.html');
- //  new Route('/Modifier un service', 'Modifier un service', '/pages/modifier un service.html');
- //  new Route('/Modifier un employé', 'Modifier un employé', '/pages/modifier un employé.html')];
- //new Route('/Contact','Contact', '/pages/contact.html');
-
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Garage V. Parrot";
