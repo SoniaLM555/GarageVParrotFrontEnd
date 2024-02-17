1c8bae61a84135046f8ee0ffdb75cef6eb625759
Ladhari Sonia <soniamessabis@hotmail.fr>
2024-02-17 09:14:08 +0100

    Correctif routage des pages change (vs update) et des liens/boutons

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,9 +9,9 @@ export const allRoutes = [
     new Route("/contact","Contact","/pages/contact.html"),
     new Route("/employeeCount", "Espace employés", "/pages/employeeCount.html"),
     new Route("/adminCount", "Espace Administrateur", "/pages/AdminCount.html"),
-    new Route("/updateService", "Modifier un service", "/pages/updateService.html"),
-    new Route("/updateEmployee", "Modifier un employé", "/pages/updateEmployee.html"),
-    new Route("/updateVehicle", "Modifier un véhicule", "/pages/updateVehicle.html")
+    new Route("/changeService", "Modifier un service", "/pages/changeService.html"),
+    new Route("/changeEmployee", "Modifier un employé", "/pages/changeEmployee.html"),
+    new Route("/changeVehicle", "Modifier un véhicule", "/pages/changeVehicle.html")
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
