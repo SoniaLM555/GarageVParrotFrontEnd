eb492b6c1edd8a6b874218d73667feb0699a0e43
Ladhari Sonia <soniamessabis@hotmail.fr>
2024-02-17 17:27:38 +0100

    Création page editpassword + correctifs se connecter

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,14 +4,15 @@ import Route from "./Route.js";
 // Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin","Connexion","/pages/signin.html"),
+    new Route("/signin","Se connecter","/pages/signin.html"),
     new Route("/forgotPassword","Mot de passe oublié", "/pages/forgotPassword.html"),
-    new Route("/contact","Contact","/pages/contact.html"),
+    new Route("/contact","Nous contacter","/pages/contact.html"),
     new Route("/employeeAccount", "Espace employés", "/pages/employeeAccount.html"),
     new Route("/adminAccount", "Espace Administrateur", "/pages/adminAccount.html"),
     new Route("/changeService", "Modifier un service", "/pages/changeService.html"),
     new Route("/changeEmployee", "Modifier un employé", "/pages/changeEmployee.html"),
-    new Route("/changeVehicle", "Modifier un véhicule", "/pages/changeVehicle.html")
+    new Route("/changeVehicle", "Modifier un véhicule", "/pages/changeVehicle.html"),
+    new Route("/editPassword", "Changer le mot de passe", "/pages/editPassword.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
