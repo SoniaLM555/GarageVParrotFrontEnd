d01abf89ca7b6409bdc674e9833556c6a936b6be
Ladhari Sonia <soniamessabis@hotmail.fr>
2024-02-17 13:09:22 +0100

    Mise en page employeeAccount

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,8 +7,8 @@ export const allRoutes = [
     new Route("/signin","Connexion","/pages/signin.html"),
     new Route("/forgotPassword","Mot de passe oublié", "/pages/forgotPassword.html"),
     new Route("/contact","Contact","/pages/contact.html"),
-    new Route("/employeeCount", "Espace employés", "/pages/employeeCount.html"),
-    new Route("/adminCount", "Espace Administrateur", "/pages/AdminCount.html"),
+    new Route("/employeeAccount", "Espace employés", "/pages/employeeAccount.html"),
+    new Route("/adminAccount", "Espace Administrateur", "/pages/adminAccount.html"),
     new Route("/changeService", "Modifier un service", "/pages/changeService.html"),
     new Route("/changeEmployee", "Modifier un employé", "/pages/changeEmployee.html"),
     new Route("/changeVehicle", "Modifier un véhicule", "/pages/changeVehicle.html")
