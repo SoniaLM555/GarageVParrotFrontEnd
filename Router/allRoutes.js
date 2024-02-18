8025beaade40ea75eb0e5f90abbc37cc541e494b
Ladhari Sonia <soniamessabis@hotmail.fr>
2024-02-18 10:19:42 +0100

    dossier auth + package json doublon supprimé + fichier js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,15 +4,15 @@ import Route from "./Route.js";
 // Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/signin","Se connecter","/pages/signin.html"),
-    new Route("/forgotPassword","Mot de passe oublié", "/pages/forgotPassword.html"),
+    new Route("/signin","Se connecter","/pages/auth/signin.html","/js/auth/signin.js"),
+    new Route("/forgotPassword","Mot de passe oublié", "/pages/auth/forgotPassword.html"),
     new Route("/contact","Nous contacter","/pages/contact.html"),
-    new Route("/employeeAccount", "Espace employés", "/pages/employeeAccount.html"),
-    new Route("/adminAccount", "Espace Administrateur", "/pages/adminAccount.html"),
-    new Route("/editService", "Modifier un service", "/pages/editService.html"),
-    new Route("/editEmployee", "Modifier un employé", "/pages/editEmployee.html"),
-    new Route("/editVehicle", "Modifier un véhicule", "/pages/editVehicle.html"),
-    new Route("/editPassword", "Changer le mot de passe", "/pages/editPassword.html"),
+    new Route("/employeeAccount", "Espace employés", "/pages/auth/employeeAccount.html"),
+    new Route("/adminAccount", "Espace Administrateur", "/pages/auth/adminAccount.html"),
+    new Route("/editService", "Modifier un service", "/pages/auth/editService.html"),
+    new Route("/editEmployee", "Modifier un employé", "/pages/auth/editEmployee.html"),
+    new Route("/editVehicle", "Modifier un véhicule", "/pages/auth/editVehicle.html"),
+    new Route("/editPassword", "Changer le mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
